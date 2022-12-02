a8ddbf822494ecce365629e91ecf37cf5587c922
laurenhammonds <110793564+laurenhammonds@users.noreply.github.com>
2022-12-02 08:46:59 -0600

    Capstone project

---
+++ Desktop/capstone/capstone/src/main/java/com/teksystems/capstone/database/entity/User.java
@@ -0,0 +1,63 @@
+package com.teksystems.capstone.database.entity;
+
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Getter
+//@Setter
+//@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//@Table(name="User")
+public class User {
+
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Column(name="id")
+//	private Integer id;
+//	
+//	@Column(name="username")
+//	private String username;
+//	
+//	@Column(name="password")
+//	private String password;
+//	
+//	@Column(name="first_name")
+//	private String firstName;
+//	
+//	@Column(name="last_name")
+//	private String lastName;
+//	
+//	@Column(name="email")
+//	private String email;
+//	
+//	@Column(name="phone")
+//	private String phone;
+//	
+//	@Column(name="address")
+//	private String address;
+//	
+//	@Column(name="city")
+//	private String city;
+//	
+//	@Column(name="state")
+//	private String state;
+//	
+//	@Column(name="zip")
+//	private String zip;
+//
+
+}
+
+
